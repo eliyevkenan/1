cfd78654f10233bad98d547a1a570799a96b9f22
unknown <tu7gbzmx6@code.edu.az>
2022-03-15 12:27:04 +0400

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Code-Ap203";
+            Console.WriteLine((searchname(name)));
+        }
+        static bool searchname(string name)
+        {
+            bool isUpperLetter = false;
+            bool isLowerLetter = false;
+            bool isDigit = false;
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (Char.IsUpper(name[i]))
+                {
+                    isUpperLetter = true;
+                }
+                if (Char.IsLower(name[i]))
+                {
+                    isLowerLetter = true;
+                }
+                if (Char.IsDigit(name[i]))
+                {
+                    isDigit = true;
+                }
+            }
+            return isDigit && isUpperLetter && isLowerLetter ;
+        }
+    }
+
+}
